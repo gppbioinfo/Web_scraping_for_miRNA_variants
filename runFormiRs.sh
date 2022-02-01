fb1d8f68870a0d945247f3a4978f3b91d9a31814
gppbioinfo <gppbioinfo@gmail.com>
2022-01-31 22:15:16 -0600

    Update runFormiRs.sh

--- runFormiRs.sh
+++ runFormiRs.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 ###############################################
-# All codes run into python3 or later versions.
+# All codes run into python3 or later version.
 ###############################################
 
 # Downloading miRNA specific all cancer condition data including sequence and expression.
 
 python parseTIE.py
 
-for ii in $(ls *-align.txt_uridylated.txt )
-do
+# Checking uridylated isomiRs sequences, comparing expression with canonical miRNA
+ 
+# Input folder name with metafile for each miRNA
 
-echo $ii
+for inputFile in $(ls */*-align.txt)
 
-python3.8 SampleInfo.py $ii ../public_metadata.txt $ii\.Cancer_meantissue.txt
+python CheckUnTempExp.py $inputFile $outputFile
 
-python3.8 GroupAverage.py $ii\.Cancer_meantissue.txt $ii\.Cancer_meantissueFinal.txt
+# Taking input from above script and checking samplewise expression and calculate average for each isomiRs sequences and their miRNA
+
+python SampleInfo.py $outputFile ../public_metadata.txt $outputFile.Cancer_meantissue.txt
+
+python GroupAverage.py $outputFile.Cancer_meantissue.txt $outputFile.Cancer_meantissueFinal.txt
+
+# Plot expression for each miRNA 
+
+Rscript plotChart2.R
 
 done
+
